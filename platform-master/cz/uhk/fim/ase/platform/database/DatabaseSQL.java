19a966e09f8616df4ac1e07287d521cc60852b2a
AnDylek <AnDylek@DESKTOP-2EMJ7JJ>
2016-02-09 16:27:31 +0100

    Vytvoření dvou metod pro práci s databází
    CreateDB() - která vytvořá samotnou databázi, zahodí starou (ideálně zavolat při inicializaci programu/aplikace) 
    
    WriteIntoDB(id,jméno,částka) - vytvoří nový řádek v tabulce agents, ideálně volat při každém tiku na každém agentovi, otázka jak to utáhne databáze.

--- platform-master/cz/uhk/fim/ase/platform/database/DatabaseSQL.java
+++ platform-master/cz/uhk/fim/ase/platform/database/DatabaseSQL.java
@@ -1,5 +1,61 @@
 package cz.uhk.fim.ase.platform.database;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DatabaseSQL {
 
+    public void createDB() {
+        try {
+            // load JDBC driver into memory
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            // create conenction to database
+            Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB;create=true");
+            // create instance of statement
+            Statement st = con.createStatement();
+
+            st.addBatch("DROP TABLE agenti");
+            st.addBatch("CREATE TABLE agents(id INT NOT NULL GENERATED ALWAYS AS IDENTITY " + "CONSTRAINT AGENTI_PK PRIMARY KEY, name VARCHAR(26), valut INTEGER()");
+
+            //TEST
+            st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('1','ÈEZ','15000000')");
+            ResultSet rs = st.executeQuery("SELECT * FROM agents ORDER BY id, valut");
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                Integer valut = rs.getInt("valut");
+                System.out.println(id + " | " + name + " | " + valut + "\n");
+            }
+
+            st.executeBatch();
+            st.close();
+            con.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void WriteIntoDB(String id, String name, Integer valut) {
+        try {
+            // load JDBC driver into memory
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            // create conenction to database
+            Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB");
+            // create instance of statement
+            Statement st = con.createStatement();
+            st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('" + id + "','" + name + "','" + valut + "')");            
+            st.executeBatch();
+            st.close();
+            con.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
