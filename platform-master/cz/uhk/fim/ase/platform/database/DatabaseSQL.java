39ba706fe6e30e167b60aeb61427c89f64a7f965
AnDylek <AnDylek@DESKTOP-2EMJ7JJ>
2016-02-09 20:50:40 +0100

    Drobné úpravy, přidán multicatch

--- platform-master/cz/uhk/fim/ase/platform/database/DatabaseSQL.java
+++ platform-master/cz/uhk/fim/ase/platform/database/DatabaseSQL.java
@@ -10,31 +10,28 @@ public class DatabaseSQL {
         try {
             // load JDBC driver into memory
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-            // create conenction to database
-            Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB;create=true");
             // create instance of statement
-            Statement st = con.createStatement();
-
-            st.addBatch("DROP TABLE agenti");
-            st.addBatch("CREATE TABLE agents(id INT NOT NULL GENERATED ALWAYS AS IDENTITY " + "CONSTRAINT AGENTI_PK PRIMARY KEY, name VARCHAR(26), valut INTEGER()");
-
-            //TEST
-            st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('1','ÈEZ','15000000')");
-            ResultSet rs = st.executeQuery("SELECT * FROM agents ORDER BY id, valut");
-            while (rs.next()) {
-                String id = rs.getString("id");
-                String name = rs.getString("name");
-                Integer valut = rs.getInt("valut");
-                System.out.println(id + " | " + name + " | " + valut + "\n");
+            try ( // create conenction to database
+                    Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB;create=true"); // create instance of statement
+                    Statement st = con.createStatement()) {
+                
+                st.addBatch("DROP TABLE agenti");
+                st.addBatch("CREATE TABLE agents(id INT NOT NULL GENERATED ALWAYS AS IDENTITY " + "CONSTRAINT AGENTI_PK PRIMARY KEY, name VARCHAR(26), valut INTEGER()");
+                
+                //TEST
+                st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('1','ÈEZ','15000000')");
+                ResultSet rs = st.executeQuery("SELECT * FROM agents ORDER BY id, valut");
+                while (rs.next()) {
+                    String id = rs.getString("id");
+                    String name = rs.getString("name");
+                    Integer valut = rs.getInt("valut");
+                    System.out.println(id + " | " + name + " | " + valut + "\n");
+                }
+                
+                st.executeBatch();
             }
 
-            st.executeBatch();
-            st.close();
-            con.close();
-
-        } catch (SQLException ex) {
-            Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -43,17 +40,14 @@ public class DatabaseSQL {
         try {
             // load JDBC driver into memory
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-            // create conenction to database
-            Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB");
             // create instance of statement
-            Statement st = con.createStatement();
-            st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('" + id + "','" + name + "','" + valut + "')");            
-            st.executeBatch();
-            st.close();
-            con.close();
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+            try ( // create conenction to database
+                    Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB"); // create instance of statement
+                    Statement st = con.createStatement()) {
+                st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('" + id + "','" + name + "','" + valut + "')");
+                st.executeBatch();
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
         }
 
