d852b7cded1685701acf2b377f8c9dbe4568f8e6
AnDylek <AnDylek@DESKTOP-2EMJ7JJ>
2016-02-14 10:45:33 +0100

    Propojení s reálnou databází

--- platform-master/cz/uhk/fim/ase/platform/database/DatabaseSQL.java
+++ platform-master/cz/uhk/fim/ase/platform/database/DatabaseSQL.java
@@ -4,32 +4,53 @@ import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ *
+ * @author AnDylek
+ */
 public class DatabaseSQL {
 
+    //JDBC driver and database url (hosting on wedos)
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://wm80.wedos.net/";
+
+    //database login
+    //TODO: Nebezpecne!! Nejlepe zahashovat..
+    static final String USER = "a94420_db";
+    static final String PASS = "deRuNsfq";
+
     public void createDB() {
+        Connection conn = null;
+        Statement st = null;
         try {
-            // load JDBC driver into memory
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-            // create instance of statement
-            try ( // create conenction to database
-                    Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB;create=true"); // create instance of statement
-                    Statement st = con.createStatement()) {
-                
-                st.addBatch("DROP TABLE agenti");
-                st.addBatch("CREATE TABLE agents(id INT NOT NULL GENERATED ALWAYS AS IDENTITY " + "CONSTRAINT AGENTI_PK PRIMARY KEY, name VARCHAR(26), valut INTEGER()");
-                
-                //TEST
-                st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('1','ÈEZ','15000000')");
-                ResultSet rs = st.executeQuery("SELECT * FROM agents ORDER BY id, valut");
-                while (rs.next()) {
-                    String id = rs.getString("id");
-                    String name = rs.getString("name");
-                    Integer valut = rs.getInt("valut");
-                    System.out.println(id + " | " + name + " | " + valut + "\n");
-                }
-                
-                st.executeBatch();
+
+            //register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //Execute a query
+            System.out.println("Creating statement...");
+            st = conn.createStatement();
+            String sql;
+            st.addBatch("DROP TABLE agenti");
+            st.addBatch("CREATE TABLE agents(id INT NOT NULL GENERATED ALWAYS AS IDENTITY " + "CONSTRAINT AGENTI_PK PRIMARY KEY, name VARCHAR(26), valut INTEGER()");
+
+            st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('1','ÈEZ','15000000')");
+            ResultSet rs = st.executeQuery("SELECT * FROM agents ORDER BY id, valut");
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                Integer valut = rs.getInt("valut");
+                System.out.println(id + " | " + name + " | " + valut + "\n");
             }
+            st.executeBatch();
+
+            rs.close();
+            st.close();
+            conn.close();
 
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(DatabaseSQL.class.getName()).log(Level.SEVERE, null, ex);
@@ -39,10 +60,10 @@ public class DatabaseSQL {
     public void WriteIntoDB(String id, String name, Integer valut) {
         try {
             // load JDBC driver into memory
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            Class.forName(JDBC_DRIVER);
             // create instance of statement
             try ( // create conenction to database
-                    Connection con = DriverManager.getConnection("JDBC:derby:AgentiDB"); // create instance of statement
+                    Connection con = DriverManager.getConnection(DB_URL, USER, PASS); // create instance of statement
                     Statement st = con.createStatement()) {
                 st.addBatch("INSERT INTO agents (id, name, valut) VALUES ('" + id + "','" + name + "','" + valut + "')");
                 st.executeBatch();
