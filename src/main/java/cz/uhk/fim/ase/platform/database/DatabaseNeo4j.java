3876eb6a6ee7af553e7b597e80afb4692fd61026
Radek salay <radek.salay@gmail.com>
2016-03-28 18:36:18 +0200

    vylepšení zápisu
    Úprava, která na v lokálních podmínkách funguje. nutnost v kódu měnit
    cestu k zápisu. Ta se nachází v core/Platform.java atribut path

--- src/main/java/cz/uhk/fim/ase/platform/database/DatabaseNeo4j.java
+++ src/main/java/cz/uhk/fim/ase/platform/database/DatabaseNeo4j.java
@@ -11,32 +11,48 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import cz.uhk.fim.ase.platform.model.Agent;
 
 public class DatabaseNeo4j {
-	 public static int count = 0;//declaring number of tic
-
-	public void writeToDB(String path, List<Agent> registr) {
+	public void writeNodeToDB(String path, List<Agent> registr) {
 		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
+		@SuppressWarnings("deprecation")
 		GraphDatabaseService db = dbFactory.newEmbeddedDatabase(path);
 		
-		
 		for (Agent agent : registr) {
+			//create node
 			try (Transaction tx = db.beginTx()){
-				Node newNode = db.createNode(Neo4jLabel.FIRM);//create node
+				Node newNode = db.createNode(Neo4jLabel.FIRM);
 				
-				//set proprtese
-				newNode.setProperty("IdAgenta", agent.getId());
-				newNode.setProperty("IdPlatformy", agent.getPlatform());
+				//set propertese
+				newNode.setProperty("IdAgent", agent.getId());
+				newNode.setProperty("IdPlatform", agent.getPlatform());
 				newNode.setProperty("Finance", agent.getFinenc());
-				newNode.setProperty("Produkce", agent.getProdukt());
-				
-				for (Agent agentInPartners : agent.getPartners()) {
-					//zjitit na jakém místì v seznamu je daný agent
-					int index = registr.indexOf(agentInPartners);
-					Node relNode = db.getNodeById(index);
-					newNode.createRelationshipTo(relNode, Neo4jRel.WorkWith);	
-				}
+				newNode.setProperty("Produkt", agent.getProdukt());
+				tx.success();
 			} 
 		}
+	}
+	public void writeRelationshipToDB(String path, List<Agent> registr) {
 		
+		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
+		@SuppressWarnings("deprecation")
+		GraphDatabaseService db = dbFactory.newEmbeddedDatabase(path);
 		
+		try (Transaction tx = db.beginTx()){
+			for (Agent agent : registr) {
+				//synchronise index from registr and neo4j DB
+				int indexZdrojovehoNodu = registr.indexOf(agent);
+				Node zdrojovyNode = db.getNodeById(indexZdrojovehoNodu);
+				
+				//write relationship betwene agent´s.
+				for (Agent agentuvPartner : agent.getPartners()) {
+					//synchronise index from registr and neo4j DB
+					int indexCilovehoNodu = registr.indexOf(agentuvPartner);
+					Node cilovyNode = db.getNodeById(indexCilovehoNodu);
+					
+					//create relationship betwene agent and his partner
+					zdrojovyNode.createRelationshipTo(cilovyNode, Neo4jRel.WorkWith);
+				}
+			}
+			tx.success();
+		}
 	}
 }
